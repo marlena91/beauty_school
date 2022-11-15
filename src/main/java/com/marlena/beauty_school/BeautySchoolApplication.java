46d6f6167af062ec4e1ef2fa4ad78262233df464
marlena91 <marlena.zarczynska@gmail.com>
2022-11-15 03:24:21 -0800

    Initial commit

---
+++ src/main/java/com/marlena/beauty_school/BeautySchoolApplication.java
@@ -0,0 +1,13 @@
+package com.marlena.beauty_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlena/beauty_school/BeautySchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlena.beauty_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

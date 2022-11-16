f0bcb9a905e7a287e2f6bb39851a6f3101e88b66
marlena91 <marlena.zarczynska@gmail.com>
2022-11-16 03:32:24 -0800

    Added thymeleaf and devtools to dependencies

--- src/main/java/com/marlena/beautyschool/controller/HomeController.java
+++ src/main/java/com/marlena/beautyschool/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.marlena.beautyschool.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String displayHomePage() {
+    @RequestMapping(value = {"", "/","/home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Johny Smith");
         return "home.html";
     }
 }

6797099b95288dc4a210d28181af03b65cf6d13b
marlena91 <marlena.zarczynska@gmail.com>
2022-11-23 01:55:30 -0800

    Submitted information from Contact page using POJO object

--- src/main/java/com/marlena/beautyschool/controller/ContactController.java
+++ src/main/java/com/marlena/beautyschool/controller/ContactController.java
@@ -1,10 +1,12 @@
 package com.marlena.beautyschool.controller;
 
+import com.marlena.beautyschool.model.Contact;
+import com.marlena.beautyschool.service.ContactService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -14,20 +16,21 @@ public class ContactController {
 
     private static Logger log = LoggerFactory.getLogger(ContactController.class);
 
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
     @RequestMapping("/contact")
     public String displayContactPage() {
         return "contact.html";
     }
 
     @RequestMapping(value = "/saveMsg", method = POST)
-    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String phone,
-                                    @RequestParam String email, @RequestParam String subject,
-                                    @RequestParam String message) {
-        log.info("Name: " + name);
-        log.info("Phone number: " + phone);
-        log.info("Email: " + email);
-        log.info("Subject: " + subject);
-        log.info("Message: " + message);
+    public ModelAndView saveMessage(Contact contact){
+        contactService.saveMessageDetails(contact);
         return new ModelAndView("redirect:/contact");
     }
 }
---
+++ src/main/java/com/marlena/beautyschool/model/Contact.java
@@ -0,0 +1,61 @@
+package com.marlena.beautyschool.model;
+
+public class Contact {
+
+    private String name;
+    private String phone;
+    private String email;
+    private String subject;
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", subject='" + subject + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}

81b1fbc5ba151825b51b1b199ddf7c0f5ab95f34
marlena91 <marlena.zarczynska@gmail.com>
2022-11-25 01:21:41 -0800

    Added accepting Path Params using @PathVariable annotation

--- src/main/java/com/marlena/beautyschool/controller/EventsController.java
+++ src/main/java/com/marlena/beautyschool/controller/EventsController.java
@@ -4,6 +4,7 @@ import com.marlena.beautyschool.model.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
@@ -13,12 +14,17 @@ import java.util.stream.Collectors;
 @Controller
 public class EventsController {
 
-    @GetMapping("/events")
-    public String displayEvents(@RequestParam(required = false) boolean local,
-                                @RequestParam(required = false) boolean online,Model model) {
-        model.addAttribute("local", local);
-        model.addAttribute("online", online);
+    @GetMapping("/events/{display}")
+    public String displayEvents(@PathVariable String display, Model model) {
 
+        if (null != display && display.equals("all")) {
+            model.addAttribute("local", true);
+            model.addAttribute("online", true);
+        } else if (null != display && display.equals("local")) {
+            model.addAttribute("local", true);
+        } else if (null != display && display.equals("online")) {
+            model.addAttribute("online", true);
+        }
         List<Event> events = Arrays.asList(
                 new Event(" 26 November ", "Meeting with the make-up artist, Hamburg", Event.Type.LOCAL),
                 new Event(" 27 November ", "10 questions to... monthly meeting with professionals", Event.Type.ONLINE),
@@ -35,7 +41,7 @@ public class EventsController {
         );
 
         Event.Type[] types = Event.Type.values();
-        for(Event.Type type : types) {
+        for (Event.Type type : types) {
             model.addAttribute(type.toString(),
                     (events.stream().filter(event -> event.getType().equals(type)).collect(Collectors.toList())));
         }

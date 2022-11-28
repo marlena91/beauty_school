c7029e3b50adceaee8f3a4a9356a1685f6056115
marlena91 <marlena.zarczynska@gmail.com>
2022-11-28 01:18:41 -0800

    Configured custom security configurations using Spring Security

---
+++ src/main/java/com/marlena/beautyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.marlena.beautyschool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .mvcMatchers("/home").permitAll()
+                .mvcMatchers("/holidays/**").permitAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+                .mvcMatchers("/courses").permitAll()
+                .mvcMatchers("/about").permitAll()
+                .and().formLogin().and().httpBasic();
+        return http.build();
+    }
+}

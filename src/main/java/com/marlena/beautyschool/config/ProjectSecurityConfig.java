03b00198519fb88108563b5c08fd939d806c916c
Marlena Zarczynska <marlena.zarczynska@gmail.com>
2022-12-07 13:56:28 +0100

    Setup H2 Database part1

--- src/main/java/com/marlena/beautyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/marlena/beautyschool/config/ProjectSecurityConfig.java
@@ -14,7 +14,7 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().ignoringAntMatchers("/saveMsg").and()
+        http.csrf().ignoringAntMatchers("/saveMsg").ignoringAntMatchers("/h2-console/**").and()
                 .authorizeRequests()
                 .mvcMatchers("/dashboard").authenticated()
                 .mvcMatchers("/home").permitAll()
@@ -27,7 +27,10 @@ public class ProjectSecurityConfig {
                 .and().formLogin()
                 .loginPage("/login").defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll()
                 .and().logout().logoutSuccessUrl("/login?logout=true").invalidateHttpSession(true).permitAll()
+                .and().authorizeRequests().antMatchers("/h2-console/**").permitAll()
                 .and().httpBasic();
+
+        http.headers().frameOptions().disable();
 
         return http.build();
     }

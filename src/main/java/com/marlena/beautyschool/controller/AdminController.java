30ec52b6de96be253064288d0f98db96f4d50602
Marlena Zarczynska <marlena.zarczynska@gmail.com>
2023-02-13 17:44:26 +0100

    Added displaying new Web Page on click of classes link in Dashboard

--- src/main/java/com/marlena/beautyschool/controller/AdminController.java
+++ src/main/java/com/marlena/beautyschool/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.marlena.beautyschool.controller;
 
+import com.marlena.beautyschool.model.BeautyClass;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,7 @@ public class AdminController {
     @RequestMapping("/displayClasses")
     public ModelAndView displayClasses(Model model){
         ModelAndView modelAndView = new ModelAndView("classes.html");
+        modelAndView.addObject("beautyClass", new BeautyClass());
         return modelAndView;
     }
 
